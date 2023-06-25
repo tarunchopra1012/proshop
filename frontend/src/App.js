fada7c3a08b75947458369c13644d24341778f6d
Tarun Chopra <tarun.chopra10@gmail.com>
2023-06-25 21:07:29 +0530

    Adding the Header and Footer to the site.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,18 @@
+import { Container } from "react-bootstrap";
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+
 const App = () => {
   return (
-    <p>Welcome To Proshop</p>
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome To Proshop</h1>
+        </Container>  
+      </main>
+      <Footer />
+    </>
   )
 }
 
