9ec6818969e8e3d4241132eba2c0aade119c5ec2
Tarun Chopra <tarun.chopra10@gmail.com>
2023-06-30 16:17:24 +0530

    Adding cart screen page.

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,13 @@
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { Badge, Navbar, Nav, Container } from "react-bootstrap";
 import { FaShoppingCart, FaUser  } from 'react-icons/fa';
 import { LinkContainer } from 'react-router-bootstrap';
+import { useSelector } from 'react-redux';
 import logo from '../assets/logo.png';
 
 const Header = () => {
+
+  const { cartItems } = useSelector((state) => state.cart);
+
   return (
     <header>
         <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
@@ -18,7 +22,16 @@ const Header = () => {
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
                     <LinkContainer to="/cart">
-                        <Nav.Link><FaShoppingCart /> Cart</Nav.Link>
+                        <Nav.Link>
+                            <FaShoppingCart /> Cart
+                            {
+                                cartItems.length > 0 && (
+                                    <Badge pill bg="success" style={{marginLeft: '5px'}}>
+                                        { cartItems.reduce((a, c) => a + c.qty, 0) }
+                                    </Badge>
+                                )
+                            }
+                        </Nav.Link>
                     </LinkContainer>
                     <LinkContainer to="/login">   
                         <Nav.Link href="/login"><FaUser /> Sign In</Nav.Link>
