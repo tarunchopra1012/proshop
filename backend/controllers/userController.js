7da4bf9c015bb7cc4e909f2061cc4ccb043550d1
Tarun Chopra <tarun.chopra10@gmail.com>
2023-07-12 08:05:53 +0530

    Fixing bug in user login.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,7 +7,7 @@ import User from "../models/userModel.js";
 const authUser = asyncHandler(async (req, res) => {
     const { email, password } = req.body;
 
-    const user = User.findOne({ email });
+    const user = await User.findOne({ email });
 
     if(user && (await user.matchPassword(password))) {
         res.json({
